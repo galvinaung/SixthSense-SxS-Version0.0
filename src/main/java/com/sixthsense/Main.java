a9c65613ac1447eade210753f901b3fb35587a88
Galvin Aung <galvinaung@gmail.com>
2026-07-18 23:28:52 +0630

    initialize SxS v0.0 Java application core
    Create Gradle Java project structure and first runnable SxS launcher.

---
+++ src/main/java/com/sixthsense/Main.java
@@ -0,0 +1,14 @@
+package com.sixthsense;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("================================");
+        System.out.println("  SixthSense SxS Version 0.0");
+        System.out.println("  Status: Running");
+        System.out.println("  Programmer: Kaung");
+        System.out.println("================================");
+
+    }
+}
