471868c979d23333465380ec6c822815e882aa42
Galvin Aung <galvinaung@gmail.com>
2026-07-19 19:04:55 +0630

    ...

--- src/main/java/com/sixthsense/system/SystemInfo.java
+++ src/main/java/com/sixthsense/system/SystemInfo.java
@@ -1,5 +1,8 @@
 package com.sixthsense.system;
 
+import java.lang.management.ManagementFactory;
+import com.sun.management.OperatingSystemMXBean;
+
 public class SystemInfo {
 
     public void showSystemInfo() {
@@ -13,6 +16,25 @@ public class SystemInfo {
         System.out.println("Computer Name    : " + System.getenv("COMPUTERNAME"));
         System.out.println("CPU Architecture : " + System.getProperty("os.arch"));
         System.out.println("Processors       : " + Runtime.getRuntime().availableProcessors());
+
+        OperatingSystemMXBean osBean =
+                (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+
+        long totalMemory = osBean.getTotalMemorySize();
+        long freeMemory = osBean.getFreeMemorySize();
+        long usedMemory = totalMemory - freeMemory;
+
+        System.out.println();
+        System.out.println("===== Memory Information =====");
+
+        System.out.printf("Total Memory     : %.2f GB%n",
+                totalMemory / 1024.0 / 1024 / 1024);
+
+        System.out.printf("Free Memory      : %.2f GB%n",
+                freeMemory / 1024.0 / 1024 / 1024);
+
+        System.out.printf("Used Memory      : %.2f GB%n",
+                usedMemory / 1024.0 / 1024 / 1024);
 
     }
 
