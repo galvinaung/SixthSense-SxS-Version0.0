47eb8165805a69c49789b74923f61daa0e66838f
Galvin Aung <galvinaung@gmail.com>
2026-07-19 18:54:50 +0630

    Feature 1: Display system information

--- src/main/java/com/sixthsense/system/SystemInfo.java
+++ src/main/java/com/sixthsense/system/SystemInfo.java
@@ -6,6 +6,14 @@ public class SystemInfo {
 
         System.out.println("===== System Information =====");
 
+        System.out.println("Operating System : " + System.getProperty("os.name"));
+        System.out.println("OS Version       : " + System.getProperty("os.version"));
+        System.out.println("Java Version     : " + System.getProperty("java.version"));
+        System.out.println("Username         : " + System.getProperty("user.name"));
+        System.out.println("Computer Name    : " + System.getenv("COMPUTERNAME"));
+        System.out.println("CPU Architecture : " + System.getProperty("os.arch"));
+        System.out.println("Processors       : " + Runtime.getRuntime().availableProcessors());
+
     }
 
 }
